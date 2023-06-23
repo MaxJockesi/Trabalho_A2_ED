ca750ac43483a08465f23cb6a76d2e785c66edb6
jhorn26 <jeanfh26@gmail.com>
2023-06-23 12:34:24 -0300

    Cabeçalho de todas as funções

--- tree_operations.h
+++ tree_operations.h
@@ -1,3 +1,8 @@
 #include "tree_functions.h"
 
 int heightTree(Node* ptrRoot);
+int sizeTree(Node* ptrRoot);
+void findTree(Node* ptrRoot);
+bool completeTree(Node* ptrRoot);
+bool perfectTree(Node* ptrRoot);
+void printTree(Node* ptrRoot);
