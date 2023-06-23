808a642585d4171ac54d06dddba5b9193c047265
jhorn26 <jeanfh26@gmail.com>
2023-06-23 12:35:12 -0300

    Nova função sizeTree

--- tree_operations.cpp
+++ tree_operations.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 #include "tree_operations.h"
 
+//Função que retorna a altura da árvore (quantos níveis a árvore possui)
 int heightTree(Node* ptrRoot)
 {
     //Se chegar em uma folha retorna 0, significando que chegou ao final da árvore
-    if(ptrRoot==nullptr){
+    if(ptrRoot==nullptr)
+    {
         return 0;
     }
     //Senão aumenta mais 1 na contagem da altura, e soma a maior altura entre seus filhos
-    else{
+    else
+    {
         return 1 + std::max(heightTree(ptrRoot->ptrLeft), heightTree(ptrRoot->ptrRight));
     }
 }
 
-
-
+//Função que retorna o tamnho da árvore (quantos elementos a árvore possui)
+int sizeTree(Node* ptrRoot)
+{
+    //Se chegou em uma folha, acaba a recorrência e retorna 0, significando que não há mais elementos
+    if(ptrRoot==nullptr)
+    {
+        return 0;
+    }
+    //Senão soma 1 ao tamanho mais o tamanho do galho a esquerda mais o tamnho do galho a direita
+    else
+    {
+        return 1 + sizeTree(ptrRoot) + sizeTree(ptrRoot);
+    }
+}
