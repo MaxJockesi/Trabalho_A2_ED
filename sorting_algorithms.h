6459fecfef0a38b6fb595f664fd361614a6d6645
Max Jockesi Barbosa Soares <99187584+MaxJockesi@users.noreply.github.com>
2023-06-30 10:41:37 -0300

    Header sorting_algorithms

---
+++ sorting_algorithms.h
@@ -0,0 +1,11 @@
+#ifndef SORTING_ALGORITHMS_H
+#define SORTING_ALGORITHMS_H
+
+#include <iostream>
+
+void bubbleSort(Node** ptrRoot);
+void selectionSort(Node** ptrRoot);
+void shellSort(Node** ptrRoot);
+void insertionSort(Node** ptrAddrHead);
+
+#endif
