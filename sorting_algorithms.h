95cb36d5e4e51aff1502523c2a129395f18d6c0f
Pablo Andrade Carvalho Barros <sirpablo4@gmail.com>
2023-06-30 11:44:35 -0300

    create sorting_algorithms.h

---
+++ sorting_algorithms.h
@@ -0,0 +1,12 @@
+#include "tree_functions.h"
+
+using namespace std;
+
+void swapNodes(Node**, Node*, Node*);
+void bubbleSort(Node**);
+void selectionSort(Node**);
+void putBeforeList(Node**, Node*, Node*);
+void insertionSort(Node**);
+int sizeOfList(Node*);
+Node* nodeCrawler(Node*, int, string);
+void shellSort(Node**);
